3a330bdfb0b78d34d560583a17256dd54943b65e
Daniel de Almeida Rocha <danielrocha@MacBook-Pro-de-Daniel.local>
2017-06-22 15:14:15 -0300

    implementando search por texto

--- app/src/main/java/danielrocha/mobfiq/view/activity/MainActivity.java
+++ app/src/main/java/danielrocha/mobfiq/view/activity/MainActivity.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.io.Serializable;
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements Observer {
     private MainViewModel mainViewModel;
     private ProductsListFragment productsListFragment;
     private ParamsAPI paramsAPI = new ParamsAPI();
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +50,43 @@ public class MainActivity extends AppCompatActivity implements Observer {
                 runOnUiThread( () -> mainViewModel.getCategories() );
 
         }).start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        MenuItem myActionMenuItem = menu.findItem( R.id.action_search);
+        searchView = (SearchView) myActionMenuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if(query.trim().equals("")) {
+                    Toast.makeText(MainActivity.this, getString(R.string.search_without_text_message), Toast.LENGTH_SHORT).show();
+                } else {
+                    if (!searchView.isIconified()) {
+                        searchView.setIconified(true);
+                    }
+                    myActionMenuItem.collapseActionView();
+
+                    ParamsAPI paramsAPIIntent = new ParamsAPI();
+                    paramsAPIIntent.setQuery(query);
+
+                    Intent searchResultIntent = new Intent(MainActivity.this, SearchResultActivity.class);
+                    searchResultIntent.putExtra(ProductsListFragment.API_QUERY_EXTRA, paramsAPIIntent);
+                    searchResultIntent.putExtra(SearchResultActivity.SEARCH_TITLE_EXTRA, query);
+
+                    startActivity(searchResultIntent);
+                }
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+                return false;
+            }
+        });
+        return true;
     }
 
     private void initDataBinding() {
