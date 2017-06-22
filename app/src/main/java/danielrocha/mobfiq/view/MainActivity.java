361618affa426db9aca308f4a212e55e98514b47
Daniel de Almeida Rocha <danielrocha@MacBook-Pro-de-Daniel.local>
2017-06-22 09:37:00 -0300

    adicionando libs iniciais e começando modelagem mvvm

--- app/src/main/java/danielrocha/mobfiq/view/MainActivity.java
+++ app/src/main/java/danielrocha/mobfiq/view/MainActivity.java
@@ -1,7 +1,9 @@
-package danielrocha.mobfiq;
+package danielrocha.mobfiq.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import danielrocha.mobfiq.R;
 
 public class MainActivity extends AppCompatActivity {
 
